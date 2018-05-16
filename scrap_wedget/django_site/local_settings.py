0f0b1fbd28649715f7f455e925ba5f0ff169b50a
Atizaz Abid <atizaz.abid@emblemtechnologies.com>
2018-05-17 01:41:36 +0500

    database

--- scrap_wedget/django_site/local_settings.py
+++ scrap_wedget/django_site/local_settings.py
@@ -10,31 +10,42 @@ DEBUG = True
 BASE_DIR = os.path.dirname(__file__)
 # local_database_settings
 
+# DATABASES = {
+#     'default': {
+#         'ATOMIC_REQUESTS': True,
+#         'CONN_MAX_AGE': 1000,
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'wedget',
+#         'USER': 'postgres',
+#         'PASSWORD': 'emblemtech',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     },
+# }
+# DATABASES = {
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'urmemesc_coincab_mysqldb',
+#         'USER': "urmemesc_coincab",
+#         'PASSWORD': '-*QQ!)VL5oj#',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     },
+# }
+
 DATABASES = {
     'default': {
         'ATOMIC_REQUESTS': True,
         'CONN_MAX_AGE': 1000,
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'wedget',
-        'USER': 'postgres',
-        'PASSWORD': 'emblemtech',
+        'NAME': 'coincab_db',
+        'USER': 'postgress',
+        'PASSWORD': 'letm3in',
         'HOST': 'localhost',
         'PORT': '5432',
     },
 }
-DATABASES = {
-    'default': {
-        'ATOMIC_REQUESTS': True,
-        'CONN_MAX_AGE': 1000,
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'urmemesc_coinscope',
-        'USER': 'urmemesc_postgre',
-        'PASSWORD': 'TeamTechGuru',
-        'HOST': 'urmemes.com',
-        'PORT': '3306',
-    },
-}
-
 
 
 MEDIA_URL = '/media/'
