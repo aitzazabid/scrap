853fd4a35893fca373c8ff267d212f6b37702aab
Atizaz Abid <atizaz.abid@emblemtechnologies.com>
2018-05-21 05:28:48 +0500

    added the charts

--- scrap_wedget/django_site/webadmin/templatetags/admin_tags.py
+++ scrap_wedget/django_site/webadmin/templatetags/admin_tags.py
@@ -2,15 +2,25 @@ from django import template
 from client.models import CurrencyGraph_values
 register = template.Library()
 
+import random
+import json
+from django.utils.safestring import mark_safe
 
 
 @register.filter()
 def graph_values(name):
 
     name = name
-
-    CGV = CurrencyGraph_values.objects.filter(is_removed=False,name=name)
     list = []
-    for values in CGV:
-        list.append(float(values.price))
+    for i in range(0,7):
+        r =  random.randint(1, 9)
+        list.append(r)
+    # return mark_safe(json.dumps(list))
+
     return list
+
+    # CGV = CurrencyGraph_values.objects.filter(is_removed=False,name=name)
+    # list = []
+    # for values in CGV:
+    #     list.append(float(values.price))
+
